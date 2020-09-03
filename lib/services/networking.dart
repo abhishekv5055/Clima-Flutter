dcaffea9ea7d5ee621103c602bed827f3ebfde54
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-09-03 17:37:04 +0530

    networking.dart completed

---
+++ lib/services/networking.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class NetworkingHelper {
+  final String url;
+
+  NetworkingHelper({this.url});
+
+  Future getData() async {
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
